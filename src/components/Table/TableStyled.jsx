bc209447d5c664d48e2ab1ccbcdde2f827285163
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-04-08 20:40:06 -0300

    chore

--- src/components/Table/TableStyled.jsx
+++ src/components/Table/TableStyled.jsx
@@ -1,10 +1,39 @@
 import styled from 'styled-components'
 
+export const Main = styled.main`
+  // display: block;
+  // margin: 0 auto;
+`
+
+export const Form = styled.form`
+  input {
+    width: 162px;
+    // width: 7em;
+    height: 1.7em;
+    font-size: 1.3em;
+    margin-bottom: 1em;
+  }
+  button {
+    width: 110px;
+    // width: 6em;
+    height: 2em;
+    color: white;
+    background-color: #4985E8;
+    font-size: 1.3em;
+    border: none;
+    cursor: pointer;
+    border-radius: 2px;
+  }
+`
+
 export const Table = styled.table`
-  border: 1px solid black;
+  
+  text-align: center;
 
   td {
     padding: 10px;
     border: 1px solid black;
+    // text-align: center;
   }
+  
 `
--- src/pages/ChallengeStyled.jsx
+++ src/pages/ChallengeStyled.jsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 export const Main = styled.main`
-  max-width: 95%;
-  margin: auto;
+  max-width: 90%;
+  margin: 0 auto;
 
   h1 {
     font-size: 2em;
@@ -17,29 +17,7 @@ export const Main = styled.main`
     margin-bottom: 1em;
   }
 
-  form {
-    display: flex;
-  }
-
-  input {
-    width: 7em;
-    height: 1.7em;
-    font-size: 1.3em;
-  }
-
-  button {
-    width: 6em;
-    height: 2em;
-    color: white;
-    background-color: #4985E8;
-    font-size: 1.3em;
-    border: none;
-    cursor: pointer;
-    border-radius: 2px;
-  }
-
   @media (min-width: 1100px) {
     max-width: 90%;
   }
-
 `
