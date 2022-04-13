b3575f3836e3295d76ea7d81d490b4f6ee3c3435
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-04-12 22:03:22 -0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/52-semanas-react'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
